edcb6cfc1167fbd16daf398ef0d60f1a80db7f62
Rupam Bhattacharya <rupam.speaks@gmail.com>
2017-11-20 08:26:52 +0100

    Create model.py
    Application of Grid LSTM to Predictive Maintenance

---
+++ model.py
@@ -0,0 +1,147 @@
+import argparse
+import os
+import pickle
+import time
+import keras
+
+import pandas as pd
+import tensorflow as tf
+
+from model import Model
+from utils import TextLoader
+
+# Setting seed for reproducability
+np.random.seed(1234)  
+PYTHONHASHSEED = 0
+from sklearn import preprocessing
+from sklearn.metrics import confusion_matrix, recall_score, precision_score
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, LSTM, Activation
+
+# read training data 
+train_df = pd.read_csv('PM_train.txt', sep=" ", header=None)
+train_df.drop(train_df.columns[[26, 27]], axis=1, inplace=True)
+train_df.columns = ['id', 'cycle', 'setting1', 'setting2', 'setting3', 's1', 's2', 's3',
+                     's4', 's5', 's6', 's7', 's8', 's9', 's10', 's11', 's12', 's13', 's14',
+                     's15', 's16', 's17', 's18', 's19', 's20', 's21']
+                     
+# read test data
+test_df = pd.read_csv('PM_test.txt', sep=" ", header=None)
+test_df.drop(test_df.columns[[26, 27]], axis=1, inplace=True)
+test_df.columns = ['id', 'cycle', 'setting1', 'setting2', 'setting3', 's1', 's2', 's3',
+                     's4', 's5', 's6', 's7', 's8', 's9', 's10', 's11', 's12', 's13', 's14',
+                     's15', 's16', 's17', 's18', 's19', 's20', 's21']
+                
+# MinMax normalization
+train_df['cycle_norm'] = train_df['cycle']
+cols_normalize = train_df.columns.difference(['id','cycle','RUL','label1','label2'])
+min_max_scaler = preprocessing.MinMaxScaler()
+norm_train_df = pd.DataFrame(min_max_scaler.fit_transform(train_df[cols_normalize]), 
+                             columns=cols_normalize, 
+                             index=train_df.index)
+join_df = train_df[train_df.columns.difference(cols_normalize)].join(norm_train_df)
+train_df = join_df.reindex(columns = train_df.columns)
+train_df.head()
+
+test_df['cycle_norm'] = test_df['cycle']
+norm_test_df = pd.DataFrame(min_max_scaler.transform(test_df[cols_normalize]), 
+                            columns=cols_normalize, 
+                            index=test_df.index)
+test_join_df = test_df[test_df.columns.difference(cols_normalize)].join(norm_test_df)
+test_df = test_join_df.reindex(columns = test_df.columns)
+test_df = test_df.reset_index(drop=True)
+test_df.head()
+
+# pick a large window size of 50 cycles
+sequence_length = 50
+
+# function to reshape features into (samples, time steps, features) 
+def gen_sequence(id_df, seq_length, seq_cols):
+    """ Only sequences that meet the window-length are considered, no padding is used. This means for testing
+    we need to drop those which are below the window-length. An alternative would be to pad sequences so that
+    we can use shorter ones """
+    data_array = id_df[seq_cols].values
+    num_elements = data_array.shape[0]
+    for start, stop in zip(range(0, num_elements-seq_length), range(seq_length, num_elements)):
+        yield data_array[start:stop, :]
+        
+# generator for the sequences
+seq_gen = (list(gen_sequence(train_df[train_df['id']==id], sequence_length, sequence_cols)) 
+           for id in train_df['id'].unique())
+           
+# generate sequences and convert to numpy array
+seq_array = np.concatenate(list(seq_gen)).astype(np.float32)
+seq_array.shape
+
+# function to generate labels
+def gen_labels(id_df, seq_length, label):
+    data_array = id_df[label].values
+    num_elements = data_array.shape[0]
+    return data_array[seq_length:num_elements, :]
+    
+# generate labels
+label_gen = [gen_labels(train_df[train_df['id']==id], sequence_length, ['label1']) 
+             for id in train_df['id'].unique()]
+label_array = np.concatenate(label_gen).astype(np.float32)
+label_array.shape
+
+# build the network
+nb_features = seq_array.shape[2]
+nb_out = label_array.shape[1]
+
+model = Sequential()
+
+model.add(GridLSTM(
+         input_shape=(sequence_length, nb_features),
+         units=100,
+         return_sequences=True))
+model.add(Dropout(0.2))
+
+model.add(GridLSTM(
+         input_shape=(sequence_length, nb_features),
+         units=100,
+         return_sequences=True))
+model.add(Dropout(0.2))
+
+model.add(LSTM(
+          units=50,
+          return_sequences=False))
+model.add(Dropout(0.2))
+
+model.add(Dense(units=nb_out, activation='sigmoid'))
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+%%time
+# fit the network
+model.fit(seq_array, label_array, epochs=10, batch_size=200, validation_split=0.05, verbose=1,
+          callbacks = [keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=0, patience=0, verbose=0, mode='auto')])
+          
+# make predictions and compute confusion matrix
+y_pred = model.predict_classes(seq_array,verbose=1, batch_size=200)
+y_true = label_array
+print('Confusion matrix\n- x-axis is true labels.\n- y-axis is predicted labels')
+cm = confusion_matrix(y_true, y_pred)
+cm
+
+# compute precision and recall
+precision = precision_score(y_true, y_pred)
+recall = recall_score(y_true, y_pred)
+print( 'precision = ', precision, '\n', 'recall = ', recall)
+
+seq_array_test_last = [test_df[test_df['id']==id][sequence_cols].values[-sequence_length:] 
+                       for id in test_df['id'].unique() if len(test_df[test_df['id']==id]) >= sequence_length]
+
+seq_array_test_last = np.asarray(seq_array_test_last).astype(np.float32)
+seq_array_test_last.shape
+
+label_array_test_last = test_df.groupby('id')['label1'].nth(-1)[y_mask].values
+label_array_test_last = label_array_test_last.reshape(label_array_test_last.shape[0],1).astype(np.float32)
+label_array_test_last.shape
+
+# test metrics
+scores_test = model.evaluate(seq_array_test_last, label_array_test_last, verbose=2)
+
+# compute precision and recall
+precision_test = precision_score(y_true_test, y_pred_test)
+recall_test = recall_score(y_true_test, y_pred_test)
+f1_test = 2 * (precision_test * recall_test) / (precision_test + recall_test)
