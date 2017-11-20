af54784f89b68c2cfc42731cc0b26d2c86c10caf
Rupam Bhattacharya <rupam.speaks@gmail.com>
2017-11-20 08:41:48 +0100

    Update model.py

--- model.py
+++ model.py
@@ -17,20 +17,6 @@ from sklearn import preprocessing
 from sklearn.metrics import confusion_matrix, recall_score, precision_score
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, LSTM, Activation
-
-# read training data 
-train_df = pd.read_csv('PM_train.txt', sep=" ", header=None)
-train_df.drop(train_df.columns[[26, 27]], axis=1, inplace=True)
-train_df.columns = ['id', 'cycle', 'setting1', 'setting2', 'setting3', 's1', 's2', 's3',
-                     's4', 's5', 's6', 's7', 's8', 's9', 's10', 's11', 's12', 's13', 's14',
-                     's15', 's16', 's17', 's18', 's19', 's20', 's21']
-                     
-# read test data
-test_df = pd.read_csv('PM_test.txt', sep=" ", header=None)
-test_df.drop(test_df.columns[[26, 27]], axis=1, inplace=True)
-test_df.columns = ['id', 'cycle', 'setting1', 'setting2', 'setting3', 's1', 's2', 's3',
-                     's4', 's5', 's6', 's7', 's8', 's9', 's10', 's11', 's12', 's13', 's14',
-                     's15', 's16', 's17', 's18', 's19', 's20', 's21']
                 
 # MinMax normalization
 train_df['cycle_norm'] = train_df['cycle']
